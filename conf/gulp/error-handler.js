64ce1bda2de15fd85db08975d9938d19fecbbfbd
pg <pawelgabryelewicz@users.noreply.github.com>
2018-02-28 12:15:01 +0100

    Style watcher doesn't stop working after an error, bugfixes, 1.0.3

--- conf/gulp/error-handler.js
+++
@@ -1,8 +0,0 @@
-const gutil = require('gulp-util');
-
-exports = function (title) {
-  return function (err) {
-    gutil.log(gutil.colors.red(`[${title}]`), err.toString());
-    this.emit('end');
-  };
-};
